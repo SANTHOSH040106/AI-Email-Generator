bfe5dabf414dd0897a3c63c982a29e5d0acd7417
SANTHOSH040106 <murugansanthosh980@gmail.com>
2026-07-14 19:23:15 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-email-generator-9ocp.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
